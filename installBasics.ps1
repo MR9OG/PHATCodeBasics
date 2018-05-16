38cc30c01c9f704452204531e5b884a372053dfd
MR9OG <36446694+MR9OG@users.noreply.github.com>
2018-05-16 12:45:01 +0200

    Update installBasics.ps1

--- installBasics.ps1
+++ installBasics.ps1
@@ -1,7 +1,26 @@
+write-host "
+         //////////////  //////   / /////   / /////////// / ////////////////  / 
+         / //// //// //  // //     ////    ////// //// //  // / //// ////      
+         / //        //   // /      // /    / // /    / /        // /          
+         ////        //  // //     ////    ////     // //       ////           
+        //////////////// ///////////////  ///////////////       /////  
+        ///////////////  //////////////// ////////////////      ///// 
+        /////            /////     ////   /////     /////       /////          
+         / //            // /      // /    / //      / //       // /           
+         / //             / /      // /    / //      / /        // /           
+        /////            /////     ////   /////     /////       /////          
+" -foregroundcolor green
+write-host "installing Basic Apps via Scoop. Refer to scoop.sh for more info"
 iex (new-object net.webclient).downloadstring('https://get.scoop.sh') 
+write-host "installing git"
 scoop install git
+write-host "installing extras bucket"
 scoop bucket add extras
+write-host "installing conemu"
 scoop install conemu
+write-host "installing vscode"
 scoop install vscode
+write-host "installing azure-cli"
 scoop install azure-cli
+write-host "installing azure powershell module"
 scoop install azure-ps
