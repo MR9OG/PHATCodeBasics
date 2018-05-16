125cf66043a162dc48a30c802d6494fdd22ae7d7
MR9OG <36446694+MR9OG@users.noreply.github.com>
2018-05-16 12:37:24 +0200

    Create installBasics.ps1

---
+++ installBasics.ps1
@@ -0,0 +1,7 @@
+iex (new-object net.webclient).downloadstring('https://get.scoop.sh') 
+scoop install git
+scoop bucket add extras
+scoop install conemu
+scoop install vscode
+scoop install azure-cli
+scoop install azure-ps
